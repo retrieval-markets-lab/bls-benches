f918ef2e72659bf0c513d84fe484898eda4286b8
Alexander Camuto <45801863+alexander-camuto@users.noreply.github.com>
2022-11-24 16:36:40 +0000

    add: 512n benchmark

--- benches/verify.rs
+++ benches/verify.rs
@@ -41,6 +41,7 @@ macro_rules! bench_verify {
 bench_verify!(bench_verify_1, 1);
 bench_verify!(bench_verify_10, 10);
 bench_verify!(bench_verify_100, 100);
+bench_verify!(bench_verify_512, 512);
 bench_verify!(bench_verify_1000, 1000);
 
 criterion_group!(
@@ -48,6 +49,7 @@ criterion_group!(
     bench_verify_1,
     bench_verify_10,
     bench_verify_100,
+    bench_verify_512,
     bench_verify_1000
 );
 
