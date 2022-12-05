0d41336cba8dfa3e7536ee378e981bec42764fa8
ameanasad <amean.asad1999@gmail.com>
2022-12-04 22:36:25 -0500

    Example to import wasm code and run it

--- wasm/bls-utils/src/lib.rs
+++ wasm/bls-utils/src/lib.rs
@@ -6,6 +6,7 @@ use rand::Rng;
 use rand_core::{CryptoRng, RngCore};
 use sha2::{digest::generic_array::typenum::U48, digest::generic_array::GenericArray, Sha256};
 use bls_signatures::{verify, Signature, PublicKey};
+use wasmer::{Store, Module, Instance, Value,Function, imports};
 
 
 /// Generate a new private key.
@@ -127,6 +128,27 @@ pub fn make_sig_safe(
     ));
 
     (aggregated_signature, hashes, public_keys, messages)
+}
+
+
+fn import_wasm_module () -> &Function  {
+    let module_wat = r#"
+    (module
+    (type $t0 (func (param i32) (result i32)))
+    (func $add_one (export "add_one") (type $t0) (param $p0 i32) (result i32)
+        get_local $p0
+        i32.const 1
+        i32.add))
+    "#;
+
+    let mut store = Store::default();
+    let module = Module::new(&store, &module_wat).unwrap();
+    // The module doesn't import anything, so we create an empty import object.
+    let import_object = imports! {};
+    let instance = Instance::new( &mut store, &module, &import_object).unwrap();
+    let add_one = instance.exports.get_function("add_one").unwrap();
+
+    add_one
 }
 
 
