6b801773ae55d091330c6a2be161d8a12057d68b
ameanasad <amean.asad1999@gmail.com>
2022-12-29 14:52:53 -0500

    Remove unused files

--- wasm/bindgen-protocol/bls-utils/rust-plugin/src/export.rs
+++
@@ -1,4 +0,0 @@
-use crate::types::*;
-
-#[fp_bindgen_support::fp_export_signature]
-pub fn run_sig_verification(data: VerifyParams) -> bool;
--- wasm/bindgen-protocol/bls-utils/rust-wasmer-wasi-runtime/types.rs
+++
@@ -1,9 +0,0 @@
-#![allow(unused_imports)]
-use serde::{Deserialize, Serialize};
-
-#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
-pub struct VerifyParams {
-    pub aggregate_signature: Vec<u8>,
-    pub pub_keys: Vec<Vec<u8>>,
-    pub hashes: Vec<Vec<u8>>,
-}
